feb0d74175629bd15f278955befc729dcb5251fa
tarekul42 <tarekulrifat205@gmail.com>
2025-07-02 00:18:08 +0600

    added: basic project setup with react router and shadcn ui

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <div className="flex min-h-svh flex-col items-center justify-center">
+      <h1 className="text-3xl p-4">Minimal Library Management System</h1>
+      <Button>Click me</Button>
+    </div>
+
+    </>
+  );
+}
+
+export default App;
---
+++ src/routes/routes.tsx
@@ -0,0 +1,11 @@
+import App from "@/App";
+import {
+  createBrowserRouter,
+} from "react-router";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: App,
+  },
+]);

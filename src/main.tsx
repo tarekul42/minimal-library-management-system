3480299ecbe507448792e9e06e2f7cf9cd95f871
tarekul42 <tarekulrifat205@gmail.com>
2025-07-06 19:20:56 +0600

    redux initialized

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,15 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router";
 import { router } from "./routes/routes.tsx";
+import { Provider } from "react-redux";
+import { store } from "./redux/store.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <div className="bg-gray-950 min-h-screen">
-    <RouterProvider router={router} />
-    </div>
+    <Provider store={store}>
+      <div className="bg-gray-950 min-h-screen">
+        <RouterProvider router={router} />
+      </div>
+    </Provider>
   </StrictMode>
 );
---
+++ src/redux/store.ts
@@ -0,0 +1,12 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+export const store = configureStore({
+  reducer: {
+    // your reducers go here
+  },
+});
+
+// Infer the `RootState`,  `AppDispatch`, and `AppStore` types from the store itself
+export type RootState = ReturnType<typeof store.getState>;
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch;
